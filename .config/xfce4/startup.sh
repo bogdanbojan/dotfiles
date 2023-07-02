0c486044f6f1bce6c950ef4e3db8a39747e40de0
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-07-02 12:14:20 +0000

    Fix y axis size in xfce4

--- .config/xfce4/startup.sh
+++ .config/xfce4/startup.sh
@@ -11,8 +11,9 @@ discord &
 
 sleep 10
 
-wmctrl -r "Alacritty" -t 1 & wmctrl -r "Alacritty" -e 0,0,-1,1920,2160 &
-wmctrl -r "Signal" -t 2 & wmctrl -r "Signal" -e 0,0,-1,1920,2160 &
-wmctrl -r "Spotify" -t 2 & wmctrl -r "Spotify" -e 0,1920,-1,1920,2160 &
-wmctrl -r "Slack" -t 0 &  wmctrl -r "Slack" -e 0,0,-1,1920,2160 &
-wmctrl -r "Discord" -t 0 & wmctrl -r "Discord" -e 0,1920,-1,1920,2160 &
+# 2100, not 2160, in order to compensate for the px of the taskbar.
+wmctrl -r "Alacritty" -t 1 & wmctrl -r "Alacritty" -e 0,0,-1,1920,2100 &
+wmctrl -r "Signal" -t 2 & wmctrl -r "Signal" -e 0,0,-1,1920,2100 &
+wmctrl -r "Spotify" -t 2 & wmctrl -r "Spotify" -e 0,1920,-1,1920,2100 &
+wmctrl -r "Slack" -t 0 &  wmctrl -r "Slack" -e 0,0,-1,1920,2100 &
+wmctrl -r "Discord" -t 0 & wmctrl -r "Discord" -e 0,1920,-1,1920,2100 &
