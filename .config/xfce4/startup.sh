840d26261cb92d9ff36e554ce706dd8f040e2563
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-08-28 09:46:39 +0300

    Add strawberry and anki to wmctrl

--- .config/xfce4/startup.sh
+++ .config/xfce4/startup.sh
@@ -15,7 +15,7 @@ sleep 10
 
 # 2100, not 2160, in order to compensate for the px of the taskbar.
 wmctrl -r "Alacritty" -t 1 & wmctrl -r "Alacritty" -e 0,0,-1,1920,2100 &
-wmctrl -r "Signal" -t 2 & wmctrl -r "Signal" -e 0,0,-1,1920,2100 &
-wmctrl -r "Spotify" -t 2 & wmctrl -r "Spotify" -e 0,1920,-1,1920,2100 &
+wmctrl -r "Anki" -t 2 & wmctrl -r "Anki" -e 0,0,-1,1920,2100 &
+wmctrl -r "Strawberry" -t 2 & wmctrl -r "Strawberry" -e 0,1920,-1,1920,2100 &
 wmctrl -r "Slack" -t 0 &  wmctrl -r "Slack" -e 0,0,-1,1920,2100 &
 wmctrl -r "Discord" -t 0 & wmctrl -r "Discord" -e 0,1920,-1,1920,2100 &
