17e274954639644c2e9be7cc94361c9594b96244
Bogdan Bojan <bogdanbojan03@gmail.com>
2024-01-31 08:29:05 +0200

    Update nvim setup

---
+++ .config/nvim/after/plugin/dap.lua
@@ -0,0 +1,123 @@
+local dap_ok, dap = pcall(require, "dap")
+if not (dap_ok) then
+  print("nvim-dap not installed!")
+  return
+end
+require('dap').set_log_level('INFO')
+
+dap.configurations = {
+    go = {
+      {
+        type = "go",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
+      {
+        type = "go",
+        name = "Attach (Pick Process)",
+        mode = "local",
+        request = "attach",
+        processId = require('dap.utils').pick_process,
+      },
+      {
+        type = "go",
+        name = "Attach (127.0.0.1:9080)",
+        mode = "remote",
+        request = "attach",
+        port = "9080"
+      },
+    }
+}
+
+dap.adapters.go = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.stdpath("data") .. '/mason/bin/dlv',
+    args = { "dap", "-l", "127.0.0.1:${port}" },
+  },
+}
+
+local dap_ui_ok, ui = pcall(require, "dapui")
+if not (dap_ok and dap_ui_ok) then
+  require("notify")("dap-ui not installed!", "warning")
+  return
+end
+
+ui.setup({
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  expand_lines = vim.fn.has("nvim-0.7"),
+  layouts = {
+    {
+      elements = {
+        "scopes",
+      },
+      size = 0.3,
+      position = "right"
+    },
+    {
+      elements = {
+        "repl",
+        "breakpoints"
+      },
+      size = 0.3,
+      position = "bottom",
+    },
+  },
+  floating = {
+    max_height = nil,
+    max_width = nil,
+    border = "single",
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil,
+  },
+})
+
+
+-- vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
+
+-- Start debugging session
+vim.keymap.set("n", "<leader>ds", function()
+  dap.continue()
+  ui.toggle({})
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+end)
+
+-- Set breakpoints, get variable values, step into/out of functions, etc.
+vim.keymap.set("n", "<leader>dl", require("dap.ui.widgets").hover)
+vim.keymap.set("n", "<leader>dc", dap.continue)
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+vim.keymap.set("n", "<leader>dn", dap.step_over)
+vim.keymap.set("n", "<leader>di", dap.step_into)
+vim.keymap.set("n", "<leader>do", dap.step_out)
+vim.keymap.set("n", "<leader>dC", function()
+  dap.clear_breakpoints()
+end)
+
+-- Close debugger and clear breakpoints
+vim.keymap.set("n", "<leader>dx", function()
+  dap.clear_breakpoints()
+  ui.toggle({})
+  dap.terminate()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+end)
---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,110 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+    'tsserver',
+    'golangci_lint_ls',
+    'gopls',
+    'docker_compose_language_service',
+    'dockerls',
+    'helm_ls',
+    'lua_ls',
+    'sqlls',
+    'vimls',
+    'terraformls',
+    'html',
+    'bashls',
+    'yamlls',
+    'jedi_language_server',
+    'bufls',
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+    end
+})
+
+
+lsp.nvim_workspace()
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+-- golangci-lint-langserver
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig/configs'
+
+if not configs.golangcilsp then
+    configs.golangcilsp = {
+        default_config = {
+            cmd = { 'golangci-lint-langserver' },
+            root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+            init_options = {
+                command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json",
+                    "--issues-exit-code=1" },
+            }
+        },
+    }
+end
+lspconfig.golangci_lint_ls.setup {
+    filetypes = { 'go', 'gomod' }
+}
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
