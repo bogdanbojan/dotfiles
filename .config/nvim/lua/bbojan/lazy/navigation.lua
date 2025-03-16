51b3a2556738b5e1cd24ffad1deb44a7bf70549d
Bogdan Bojan <b.bojan@cv-library.co.uk>
2025-03-16 15:34:56 +0200

    Add flash

--- .config/nvim/lua/bbojan/lazy/navigation.lua
+++ .config/nvim/lua/bbojan/lazy/navigation.lua
@@ -11,8 +11,8 @@ return {
         "machakann/vim-sandwich",
         config = function()
             -- Disable default keymappings because they conflict with leap.
-            vim.g.sandwich_no_default_key_mappings = 1
-            vim.g.operator_sandwich_no_default_key_mappings = 1
+            -- vim.g.sandwich_no_default_key_mappings = 1
+            -- vim.g.operator_sandwich_no_default_key_mappings = 1
 
             vim.api.nvim_set_keymap('n', '<leader>sa', '<Plug>(sandwich-add)', { noremap = true })
             vim.api.nvim_set_keymap('x', '<leader>sa', '<Plug>(sandwich-add)', { noremap = true })
@@ -23,21 +23,17 @@ return {
         end
     },
     {
-        "ggandor/leap.nvim",
-        config = function()
-            -- First, setup leap with default mappings disabled
-            require('leap').setup({
-                create_default_mappings = false
-            })
-
-            -- Then manually add the mappings we want
-            vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-            vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-            -- You can also add the bidirectional mapping if you use it
-            -- vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
-        end
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
     },
-
     -- "gpanders/nvim-parinfer"
     -- use('rhysd/git-messenger.vim')
     -- use('towolf/vim-helm')
