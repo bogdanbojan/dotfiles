1d82121279dc6f81a32e3aee0218df81b492d52b
Bogdan Bojan <b.bojan@cv-library.co.uk>
2025-06-02 16:38:13 +0300

    Fix lsp shortcuts

--- .config/nvim/lua/bbojan/init.lua
+++ .config/nvim/lua/bbojan/init.lua
@@ -35,17 +35,27 @@ autocmd("BufWritePre", {
     end,
 })
 
+
 -- TODO Check these again. Have some defined in ./lazy/lsp.lua.
 autocmd('LspAttach', {
     group = bbojanGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<leader>wv", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vre", function() vim.diagnostic.setqflist() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
